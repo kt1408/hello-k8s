8021ebc88073bf7d2dc634409e4d4c6620f48925
Krishna <krishnatibrewal14@gmail.com>
2025-10-02 17:35:04 +0200

    Initial app, Dockerfile, k8s manifests

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (_req, res) => {
+  res.send('hello from kubernetes 👋 (v1.0)');
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
